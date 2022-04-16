62662bf64d409c9bb35191b1890a695cea69f4ed
jaehoonV <ljh1032112@gmail.com>
2022-04-16 12:05:22 +0900

    Study v-for

--- ex1/src/App.vue
+++ ex1/src/App.vue
@@ -1,22 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div>
-    <h4 :style="스타일">{{ products[0] }}</h4>
-    <p>{{ price1 }} 만원</p>
+  <div class="menu">
+    <a v-for="(m, i) in menus" :key="i">{{ m }}</a>
   </div>
-  <div>
-    <h4 :style="스타일">{{ products[1] }}</h4>
-    <p>{{ price2 }} 만원</p>
-  </div>
-  <div>
-    <h4 :style="스타일">{{ products[2] }}</h4>
-    <p>{{ price3 }} 만원</p>
+  <div v-for="(pd, i) in products" :key="i">
+    <h4 :style="스타일">{{ pd }}</h4>
+    <p>{{ price[i] }} 만원</p>
   </div>
   <div>
     <p>{{ db }}하는 이유</p>
     <p>HTML에 하드코딩해놓으면 나중에 변경이 어려움</p>
-    <p>vue의 실시간 자동 렌더링을 사용하기 위함</p>
+    <p>Vue의 실시간 자동 렌더링을 사용하기 위함</p>
     <p>HTML 속성도 데이터바인딩 가능 (:속성="데이터이름")</p>
+    <p>Vue의 HTML 반복문 &lt;태그 v-for="작명 in 몇회"&gt;</p>
+    <p>Vue의 HTML 반복문을 사용할 때 :key=""를 꼭 사용해야함</p>
+    <p>:key=""는 반복문을 돌린 요소를 컴퓨터가 구분하기 위해 사용</p>
   </div>
 </template>
 
@@ -25,12 +22,11 @@ export default {
   name: "App",
   data() {
     return {
-      price1: 60,
-      price2: 70,
-      price3: 80,
+      price: [60, 70, 80],
       db: "{{데이터바인딩}}",
       스타일: "color : blue",
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      menus: ["Home", "Shop", "about"],
     };
   },
   components: {},
@@ -44,6 +40,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
